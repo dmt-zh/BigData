29296772f13f45650a29f92ecf96c6748e70c31f
Dmitry Zhigalo <zhdmts@gmail.com>
2025-06-18 16:14:25 +0300

    Optimized run.sh

--- Hadoop/MapReduce/run.sh
+++ Hadoop/MapReduce/run.sh
@@ -14,8 +14,6 @@ hadoop jar "$HADOOP_MAPRED_HOME"/hadoop-streaming.jar \
 -Dmapred.job.name='MapReduce job' \
 -Dmapreduce.map.memory.mb=1024 \
 -Dmapred.reduce.tasks=1 \
--Dmapreduce.input.lineinputformat.linespermap=5000 \
--inputformat org.apache.hadoop.mapred.lib.NLineInputFormat \
 -file /home/ubuntu/MapReduce/mapper.py -mapper /home/ubuntu/MapReduce/mapper.py \
 -file /home/ubuntu/MapReduce/reducer.py -reducer /home/ubuntu/MapReduce/reducer.py \
 -input /user/ubuntu/2020 \
